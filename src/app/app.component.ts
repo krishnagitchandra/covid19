2a0ee9e51e2a7b3bd557f83f69b06ab89a9d5f57
krishna chandra <chandra.krishna666@gmail.com>
2021-08-09 16:15:16 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CoronaService} from "./services/corona.service"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'covid19';
+  constructor(private corona:CoronaService){}
+
+  countries:any;
+  country:any;
+  Confirmed!: number;
+  Recovered!: number;
+  Death!: number;
+
+  getCoronaData(){
+    this.corona.getCoronaRealtimeData(this.country).subscribe((data)=>{
+      console.log(data)
+      var index = data.length - 1;
+      this.Confirmed = data[index].Confirmed
+      this.Death = data[index].Deaths
+      this.Recovered = this.Confirmed - this.Death
+      
+    })
+  }
+  getCountry(country:any){
+    this.country = country
+  }
+
+  ngOnInit(){
+    this.corona.getCountries().subscribe((data)=>{
+      console.log(data);
+      this.countries = data;
+    })
+  }
 }
